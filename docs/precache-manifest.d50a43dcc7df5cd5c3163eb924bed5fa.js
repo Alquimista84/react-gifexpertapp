f405be56f7a9f3151fb2590f875c93f2c552ddef
Ivan Martinez <ivan.martinez.rolon@gmail.com>
2020-10-03 11:22:18 -0500

    docs agregado

---
+++ docs/precache-manifest.d50a43dcc7df5cd5c3163eb924bed5fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd96968ccbbbf71c1c9bf704aa8642f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ba25ce3dc8341f5f3d",
+    "url": "/static/css/main.30798062.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ba25ce3dc8341f5f3d",
+    "url": "/static/js/main.1bc6ef03.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
